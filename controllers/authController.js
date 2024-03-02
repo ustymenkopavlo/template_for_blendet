df94202bc04467c7cbd39d5ce2d156b80d62c338
AlexKat <katsubo79@gmail.com>
2024-03-02 16:42:59 +0200

    +lesson

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,11 @@
-export async function register(req, res) {}
+
+
+export async function register(req, res) {
+
+}
 
 export async function login(req, res) {}
 
-export function getUser(req, res) {}
+export function getUser(req, res) {
+    res.send(`Hello ${req.user.email}`)
+}
--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import express from "express";
-import app from "./db/index.js";
+import app from "./db/db.js";
 
 import authRouter from "./routers/authRouter.js";
 
 app.use(express.json());
 
 app.use("/auth", authRouter);
+
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).send(error.message);
+})
